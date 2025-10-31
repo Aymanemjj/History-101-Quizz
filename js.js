01704b68f5034aa00538e7bd54b133efda7ab2db
Aymanemjj <aymanemj588@gmail.com>
2025-10-31 18:59:31 +0100

    Update

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 /* Questions list */
 
-const questoinsList = {
+const questionsList = {
     Questions: [
     {question: "When did WW2 start", options: ["1981", "1913", "1939","1940"], answer:"1939"},
     {question: "Who gifted the statue of liberty to the USA ", options: ["England", "France", "Germany","Spain"], answer:"France"},
@@ -13,71 +13,106 @@ const questoinsList = {
 
 
 
-let answers = [];
-let x = "";
+let answers = 0;
 let i = 0; 
+const nextbt = document.getElementById("b2");
+const backbt = document.getElementById("b1");
 
 
 
-if(i < 5){
-
-    document.getElementById("b2").onclick = function(){
-        nextQuestion(i++)
+    nextbt.onclick = function(){
+        answerV();
+        
+        if(i< questionsList.Questions.length - 1) {
+             i++
+        }
+        nextQuestion(i);       
     };
 
     function nextQuestion(i){
+        
 
 
-
-    document.getElementById("question").innerHTML = questoinsList.Questions[i].question;
+    document.getElementById("question").innerHTML = questionsList.Questions[i].question;
         
         let j;
         for(j = 0; j < 4; j++){
-            document.getElementById("quizz-option-"+ j ).innerHTML = questoinsList.Questions[i].options[j];
+            document.getElementById("quizz-option-"+ j ).innerHTML = questionsList.Questions[i].options[j];
         }
-        console.log(i)  ;
+        console.log('i =',i);
+        
+       
+        if(i == questionsList.Questions.length - 1 ){
+                document.getElementById("b2").className = "fa-solid fa-circle-check fa-2xl";
+                nextbt.onclick = location.href="score.html";
+        }
         if( i == 1 ){
             document.getElementById("b1").style.visibility ="visible";
         }
 
 
-    document.getElementById("b2").addEventListener('click', function () {
         ["quizz-check-0", "quizz-check-1", "quizz-check-2", "quizz-check-3"].forEach(function(id) {
             document.getElementById(id).checked = false;
         });
-        })
-
+        
+       
     }
 
-}
 
 
 
-if(i >= 0){
-    document.getElementById("b1").onclick = function(){
 
-        previousQuestion(i--)
+
+    backbt.onclick = function(){
+       if(answers != 0){
+        answers--;
+       }
+       console.log("answers=",answers);
+       
+        
+        if(i>0) {
+             i--;
+        }
+        previousQuestion(i);
+        
     };
 
     function previousQuestion(i){
 
-    document.getElementById("question").innerHTML = questoinsList.Questions[i].question;
+    document.getElementById("question").innerHTML = questionsList.Questions[i].question;
         
         let j;
         for(j = 0; j < 4; j++){
-            document.getElementById("quizz-option-"+ j ).innerHTML = questoinsList.Questions[i].options[j];
+            document.getElementById("quizz-option-"+ j ).innerHTML = questionsList.Questions[i].options[j];
         }
-        console.log(i)  ;
+        console.log('i =',i);
+
+        if(i == questionsList.Questions.length - 2){
+
+            document.getElementById("b2").className = "fa-solid fa-square-caret-right fa-2xl";
+
+        }
         if( i == 0 ){
             document.getElementById("b1").style.visibility ="hidden";
         }
 
-    document.getElementById("b2").addEventListener('click', function () {
+    
+
         ["quizz-check-0", "quizz-check-1", "quizz-check-2", "quizz-check-3"].forEach(function(id) {
             document.getElementById(id).checked = false;
         });
-        })
+        
 
     }
 
-}
+   function answerV(){
+        ["quizz-check-0", "quizz-check-1", "quizz-check-2", "quizz-check-3"].forEach(function(id) {
+            if(document.getElementById(id).checked === true){
+                if(document.getElementById(id).nextElementSibling.innerHTML == questionsList.Questions[i].answer){
+                    answers++;
+                }
+            }
+        });
+        console.log("answers=",answers);
+         document.getElementById("score-value").innerHTML = answers,"/5";
+    }
